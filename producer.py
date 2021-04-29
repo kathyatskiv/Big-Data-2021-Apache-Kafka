9875cc60a3ebb12c37979df7db48995f6cc65d73
Yatskiv Kath <iamkate4@gmail.com>
2021-04-29 21:38:45 +0300

    small fixes

--- producer.py
+++ producer.py
@@ -3,27 +3,23 @@ from json import dumps
 from kafka import KafkaProducer, KafkaConsumer
 from kafka.admin import KafkaAdminClient, NewTopic
 
-producer = KafkaProducer(bootstrap_servers=['<brocker-1-id>:9092', '<brocker-2-id>:9092', '<brocker-3-id>:9092'],
+from consumer import resubscribe
+
+producer = KafkaProducer(bootstrap_servers=['<broker_1_id>:9092', '<broker_2_id>:9092', '<broker_3_id>:9092'],
                         value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 admin = KafkaAdminClient(
-    bootstrap_servers=['<brocker-1-id>:9092', '<brocker-2-id>:9092', '<brocker-3-id>:9092'], 
+    bootstrap_servers=['<broker_1_id>:9092', '<broker_2_id>:9092', '<broker_3_id>:9092'], 
     client_id='admin'
 )
 
 admin_consumer = KafkaConsumer(
-     bootstrap_servers=['<brocker-1-id>:9092', '<brocker-2-id>:9092', '<brocker-3-id>:9092'],
+     bootstrap_servers=['<broker_1_id>:9092', '<broker_2_id>:9092', '<broker_3_id>:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group')
 
 all_topics = admin_consumer.topics()
-print(all_topics)
-
-# topic_list = []
-# topic_list.append(NewTopic(name="demo-topic-2", num_partitions=1, replication_factor=3))
-# admin.create_topics(new_topics=topic_list, validate_only=False)
-
 
 def produce(tweets) :
     for t in tweets:
@@ -35,6 +31,8 @@ def produce(tweets) :
             admin.create_topics(new_topics=[new_topic], validate_only=False)
             all_topics.add(t['author_id'])
 
+            resubscribe(admin_consumer)
+
         data = {
                 'tweet_id' : t['tweet_id'],
                 'created_at' : t['created_at'],
@@ -43,5 +41,6 @@ def produce(tweets) :
                 'in_response_to_tweet_id' : t['in_response_to_tweet_id']
                 }
 
-        producer.send(t['author_id'], value=data)
+        producer.send(t['author_id'], value=data, partition=0)
     print("Total amount: ", len(tweets))
+
