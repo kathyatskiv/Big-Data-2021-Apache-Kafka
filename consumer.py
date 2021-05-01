9335a58ed1d44fd97b2e04972f8d54652bf1e8d7
Yatskiv Kath <iamkate4@gmail.com>
2021-05-01 22:29:38 +0300

    fixed errors

--- consumer.py
+++ consumer.py
@@ -4,12 +4,14 @@ import datetime
 
 from kafka import KafkaConsumer, TopicPartition
 
+from upload_data import upload
+
 
 data = {}
 
 def main():
     consumer = KafkaConsumer(
-     bootstrap_servers=['<broker_1_id>:9092', '<broker_2_id>:9092', '<broker_3_id>:9092'],
+     bootstrap_servers=['<brocker_id>:9092', '<brocker_id>:9092', '<brocker_id>:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=True)
 
@@ -20,39 +22,37 @@ def main():
 
         data['accounts'] = []
         data['accounts'].append(list(all_accounts))
+        print("get_all_acounts LOADED")
     except Exception as e:
-        print("get_all_acounts")
+        print("get_all_acounts FAILED")
         print(e)
-    finally:
-        print("get_all_acounts loaded")
-        print(data['accounts'])
 
 
-
-   
-    get_tweets_for_top_10_accounts(consumer)    
-    print("get_tweets_for_top_10_accounts loaded")
+    try:
+        get_tweets_for_top_10_accounts(consumer)    
+        print("get_tweets_for_top_10_accounts LOADED")
+    except Exception as e:
+        print("get_tweets_for_top_10_accounts FAILED")
+        print(e)
 
     try:
         get_top_20_accounts(consumer,1)
+        print("get_top_20_accounts LOADED")
     except Exception as e:
-        print("get_top_20_accounts")
+        print("get_top_20_accounts FAILED")
         print(e)
-    finally:
-        print("get_top_20_accounts loaded")
-        print(data['top20_producing_accounts'])
-
+    
     try:
         get_aggregated_statistics(consumer)
+        print("get_aggregated_statistics LOADED")
     except Exception as e:
-        print("get_aggregated_statistics")
+        print("get_aggregated_statistics FAILED")
         print(e)
-    finally:
-        print("get_aggregated_statistics loaded")
-        print(data['aggregated_statistics'])
 
     with open('data.json', 'w') as outfile:
         json.dump(data, outfile)
+
+    #upload('data.json')
 
     consumer.close()
 
@@ -78,25 +78,56 @@ def get_tweets_for_top_10_accounts(consumer):
     for topic in all_topics:
         tp = TopicPartition(topic, 0)
         consumer.seek_to_end(tp)
-        last_offset = consumer.position(tp)
+        last_offset = int(consumer.position(tp))
 
         offset = consumer.offsets_for_times({tp: start_date})
 
         start_offset = 0
         if list(offset.values())[0]:
-            start_offset = list(offset.values())[0].offset
+            start_offset = int(list(offset.values())[0].offset)
             
 
-        top10.append({'topic' : topic, 'length' : last_offset - start_offset})
+        top10.append({'user_id' : topic, 'amount' : int(last_offset - start_offset), 'length' : last_offset})
 
 
-    sorted(top10, key = lambda x: top10['length'])
+    sorted(top10, key = lambda x: x['amount'])
     top10 = top10[:10]
-    print(top10)
+
+    data['top10_producing_account_latest_tweets'] = []
+
+    for topic in top10:
+        tp = TopicPartition(topic['user_id'], 0)
+
+        if topic['length'] >= 10:
+            consumer.seek(tp, topic['length'] - 10)
+        else:
+            consumer.seek_to_beginning(tp)
+
+        messages = consumer.poll(2000,10)
+        messages = list(messages.values())
+
+        tweets = []
+
+        for message in messages:
+            tweets.append(message[0].value.decode("utf-8"))
+
+        print(tweets)
+
+        data['top10_producing_account_latest_tweets'].append(
+            {
+                'user_id' : topic['user_id'], 
+                'latest_tweets' : tweets
+            })
+
+
+    
+
 
 def get_top_20_accounts(consumer, n):
     resubscribe(consumer)
     all_topics = get_all_acounts(consumer)
+
+    data["top20_producing_accounts"] = []
 
     top20 = []
     start_date = datetime.datetime.timestamp(datetime.datetime.now() - datetime.timedelta(hours=n))
@@ -104,22 +135,25 @@ def get_top_20_accounts(consumer, n):
     for topic in all_topics:
         tp = TopicPartition(topic, 0)
         consumer.seek_to_end(tp)
-        last_offset = consumer.position(tp)
+        last_offset = int(consumer.position(tp))
 
         offset = consumer.offsets_for_times({tp: start_date})
 
         start_offset = 0
         if list(offset.values())[0]:
-            start_offset = list(offset.values())[0].offset
+            start_offset = int(list(offset.values())[0].offset)
             
 
-        top20.append({'topic' : topic, 'length' : last_offset - start_offset})
+        top20.append({'user_id' : topic, 'amount' : int(last_offset - start_offset)})
 
 
-    sorted(top20, key = lambda x: top20['length'])
+    sorted(top20, key = lambda x: x['amount'])
     top20 = top20[:20]
+
+    print(top20)
     
-    data["top20_producing_accounts"] = top20
+    for account in top20:
+        data["top20_producing_accounts"].append(account)
 
 def get_aggregated_statistics(consumer):
     resubscribe(consumer)
@@ -154,10 +188,10 @@ def get_aggregated_statistics(consumer):
 
         data['aggregated_statistics'].append(
             {
-                'topic' : topic, 
+                'user_id' : topic, 
                 '1 hour before' : last_offset - start_minus_1, 
                 '2 hours before' : start_minus_1 - start_minus_2,
-                '3 hour before' : start_minus_2 - start_minus_3
+                '3 hours before' : start_minus_2 - start_minus_3
             })
         
     
